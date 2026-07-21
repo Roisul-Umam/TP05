00c066ca95af88b1ed22cd0cf5056651f73242e5
Rois <roisul.umam@ui.ac.id>
2026-07-21 18:51:26 +0700

    Fixing error #2

--- TP5-BurhanCup/test/TournamentManagerTest.java
+++ TP5-BurhanCup/test/TournamentManagerTest.java
@@ -1,11 +1,6 @@
-package test;
 import static org.junit.Assert.*;
 import org.junit.Test;
-import Main;
-import Tim;
-import TimFakultas;
-import TimUndangan;
-import TurnamenManager;
+import java.io.IOException;
 /**
  * Tulis unit test kalian di sini. Minimal satu happy path + satu edge case
  * per method wajib (lihat TP5.docx bagian Sub-Task 1, 2, dan 3).
@@ -21,6 +16,7 @@ public class TournamentManagerTest {
         TournamentManager manager = new TournamentManager(tim);
 
         // TODO: isi golPertandingan lewat cara yang kalian sediakan
+        
         // (misalnya lewat constructor lain, setter, atau muatDataGol()),
         // lalu assert hasil totalGol() sesuai ekspektasi.
 
@@ -28,9 +24,65 @@ public class TournamentManagerTest {
     }
 
     // TODO: totalGolTurnamen() -- happy path + pastikan memanggil totalGol()
+    @Test
+    public void totalGolTurnamen_menjumlahkanSeluruhGolSemuaTim() {
+        Tim[] tim = {
+            new TimFakultas("Tim Contoh 1"),
+            new TimFakultas("Tim Contoh 2")
+        };
+        TournamentManager manager = new TournamentManager(tim);
+        assertEquals(18, manager.totalGolTurnamen());
+    }
     // TODO: pertandinganTerbaik() -- happy path + tim dengan 0 pertandingan -> -1
+    @Test
+    public void pertandinganTerbaik_mencariIndexGolTerbanyakSatuTim() {
+        Tim[] tim = { new TimFakultas("Tim Contoh") };
+        TournamentManager manager = new TournamentManager(tim);
+        assertEquals(2, manager.pertandinganTerbaik(0));
+    }
     // TODO: rataRataGol() -- happy path (pembulatan ke bawah) + tim dengan 0 pertandingan -> 0
+    @Test
+    public void rataRataGol_menghitungRataRataGolSatuTim() {
+        Tim[] tim = { new TimFakultas("Tim Contoh") };
+        TournamentManager manager = new TournamentManager(tim);
+        assertEquals(3, manager.rataRataGol(0));
+    }
     // TODO: timGolTerbanyak() -- happy path + kasus seri -> index terkecil
+    @Test
+    public void timGolTerbanyak_mencariIndexTimDenganTotalGolTertinggi() {
+        Tim[] tim = {
+            new TimFakultas("Tim Contoh 1"),
+            new TimFakultas("Tim Contoh 2")
+        };
+        TournamentManager manager = new TournamentManager(tim);
+        assertEquals(0, manager.timGolTerbanyak());
+    }
     // TODO: totalBiayaPendaftaran() -- campuran TimFakultas & TimUndangan
+    @Test
+    public void totalBiayaPendaftaran_menjumlahkanBiayaPendaftaranSemuaTim() {
+        Tim[] tim = {
+            new TimFakultas("Tim Contoh 1"),
+            new TimUndangan("Tim Contoh 2", 100),
+            new TimFakultas("Tim Contoh 3"),
+            new TimUndangan("Tim Contoh 4", 200)
+        };
+        TournamentManager manager = new TournamentManager(tim);
+        assertEquals(300, manager.totalBiayaPendaftaran());
+    }
     // TODO: muatDataGol() -- baca file kecil contoh, verifikasi isi golPertandingan
+    @Test
+    public void muatDataGol_membacaFileDanMengisiGolPertandingan() {
+        Tim[] tim = {
+            new TimFakultas("Tim Contoh 1"),
+            new TimFakultas("Tim Contoh 2")
+        };
+        TournamentManager manager = new TournamentManager(tim);
+        try {
+            manager.muatDataGol("gol.txt");
+        } catch (IOException e) {
+            fail("IOException terjadi saat membaca file: " + e.getMessage());
+        }
+        assertEquals(3, manager.totalGol(0));
+        assertEquals(5, manager.totalGol(1));
+    }
 }
